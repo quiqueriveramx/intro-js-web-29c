6be3e1abf363fa0f6049efaac21de1f433a5f2b9
Diego Lechuga <diegolechuga2003@gmail.comgit config --list>
2023-09-26 19:57:17 -0600

    feat: ciclos

--- 6-ciclos/main.js
+++ 6-ciclos/main.js
@@ -3,6 +3,38 @@ let userInput;
 
 while(!(userInput=="")){
     userInput = prompt("Ingresa un caracter");
-    arreglo.push(userInput)
+    arreglo.push(userInput);
 }
-console.log("Introduciste esto: " + arreglo)
+console.log("Introduciste esto: " + arreglo);
+
+
+let i = 0
+do {
+    console.log(i);
+    i++;
+} while(i<10)
+
+let frutas = ["Manzana", "Naranja", "Fresa", "Pera", "Uva"]
+
+console.log(frutas.length);
+
+for(let i = 0; i < frutas.length; i++){
+    console.log(frutas[i]);
+}
+
+
+let array = [4,"dos", 8, "tres", 5, 9, 1, "cero"];
+
+for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === "number") {
+        console.log(array[i]);
+    }
+}
+
+let numeros = []
+
+for(let i = 0; i <= 10; i++){
+    numeros.push(i)
+}
+
+console.log(numeros);
